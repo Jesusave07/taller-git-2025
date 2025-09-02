0bd2f56229010cc7d02ba191ad0ce152aba94a23
Adrian Palma <adrianpalmaacosta09@gmail.comgit config --listgit versiongit clean -n>
2025-09-02 14:22:58 +0200

    Agrega programa 2numeros.py

---
+++ 2numeros.py
@@ -0,0 +1,7 @@
+# Solicita al usuario dos números
+numero1 = input("Introduce el primer número: ")
+numero2 = input("Introduce el segundo número: ")
+
+# Muestra los números introducidos
+print("Has introducido:", numero1, "y", numero2)
+

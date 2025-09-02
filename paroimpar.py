3836b171ce1b69576d338f796176161edf37499f
Adrian Palma <adrianpalmaacosta09@gmail.comgit config --listgit versiongit clean -n>
2025-09-02 14:55:51 +0200

    Programa para pedir un numero y ver si es par o impar

---
+++ paroimpar.py
@@ -0,0 +1,9 @@
+# Solicita un número al usuario
+numero = int(input("Introduce un número: "))
+
+# Verifica si el número es par o impar
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
+

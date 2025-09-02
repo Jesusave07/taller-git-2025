fd6de32cdca89259d17d1ed1ce10094b439b463d
adrianpalmaacosta09-lang <adrianpalmaacosta09@gmail.com>
2025-09-02 19:16:44 +0200

    Update listadetareas.py
    Commit changes

--- listadetareas.py
+++ listadetareas.py
@@ -1,11 +1,1 @@
-# Lista de tareas
-tareas = ["Estudiar", "Comprar", "Limpiar" , "lavar ropa"]
 
-# Función para mostrar las tareas
-def mostrar_tareas():
-    print("Lista de tareas:")
-    for tarea in tareas:
-        print(f"- {tarea}")
-
-# Llamamos a la función para mostrar las tareas
-mostrar_tareas()
